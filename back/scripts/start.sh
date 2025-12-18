04a8ca37ba8b223803d493bc0169076fd90a7cca
Sihyeon0123 <snc100194@gmail.com>
2025-12-18 16:50:35 +0900

    스크립트 수정

--- back/scripts/start.sh
+++ back/scripts/start.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 set -e
 
-APP_DIR=/home/ubuntu/app/back
+APP_DIR=/home/ubuntu/app
 cd $APP_DIR
 
-# 기존 실행 중인 프로세스 종료 (있으면)
 PID=$(pgrep -f '.war' || true)
 if [ -n "$PID" ]; then
   kill -15 $PID
   sleep 5
 fi
 
-# WAR 파일 하나 선택 (bootWar 기준)
 WAR_FILE=$(ls *.war | head -n 1)
 
-# 백그라운드 실행
 nohup java -jar "$WAR_FILE" > app.log 2>&1 &
