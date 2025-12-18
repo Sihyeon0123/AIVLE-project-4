6b1d47bc0e5a97e5d709965a6d6646db7749975c
Sihyeon0123 <snc100194@gmail.com>
2025-12-18 22:27:13 +0900

    Update start.sh

--- front/scripts/start.sh
+++ front/scripts/start.sh
@@ -5,13 +5,10 @@ APP_DIR="/home/ubuntu/app"
 PM2="/usr/bin/pm2"
 NODE="/usr/bin/node"
 
-cd "$APP_DIR"
+cd "$APP_DIR/.next/standalone"
 
 $NODE -v
 $PM2 -v
 
-# 기존 프로세스 제거
 $PM2 delete front || true
-
-# standalone 서버 실행
-$PM2 start .next/standalone/server.js --name front
+$PM2 start server.js --name front --cwd "$APP_DIR/.next/standalone"
