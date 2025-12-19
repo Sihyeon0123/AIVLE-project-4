93fb60499f5786bb64966485ac316a23aaecaf0f
Sihyeon0123 <snc100194@gmail.com>
2025-12-19 11:20:58 +0900

    백엔드 배포 수정

--- back/scripts/start.sh
+++ back/scripts/start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 
 export JWT_SECRET="SecretKey123456SecretKey123456SecretKey123456"
 export DB_USERNAME="sa"
@@ -7,12 +8,19 @@ export DB_PASSWORD="1234"
 APP_DIR=/home/ubuntu/app
 cd $APP_DIR || exit 1
 
+echo "=== Stop existing WAR ==="
 PID=$(pgrep -f '.war' || true)
 if [ -n "$PID" ]; then
   kill -15 $PID
   sleep 5
 fi
 
+echo "=== Start new WAR ==="
 WAR_FILE=$(ls *.war | head -n 1)
+
+if [ -z "$WAR_FILE" ]; then
+  echo "❌ WAR file not found" >> app.log
+  exit 1
+fi
 
 /usr/bin/java -jar "$WAR_FILE" >> app.log 2>&1 &
--- back/scripts/stop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-PID=$(pgrep -f '.war' || true)
-if [ -n "$PID" ]; then
-  kill -15 $PID
-  sleep 5
-fi
